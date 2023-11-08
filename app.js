171e84fa528c652364ed9b44d2af1e9f429e12a1
giusmili <giusmili67@gmail.com>
2023-11-08 10:09:48 +0100

    js

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let btn = document.querySelector(".btn-info");
+
+    
+    btn.addEventListener("click",()=>{
+        window.open("https://www.mozilla.org/", "blank");
+    })
+})
